89b5975327f488d8c366db8078f4e2cfd6a0d0c0
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-05-15 11:50:52 +0200

    ajout de la class database

---
+++ src/models/database.js
@@ -0,0 +1,62 @@
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+class Database{
+    static instance;
+    #connection;
+
+    constructor(){
+        if (Database.instance) {
+            return Database.instance;
+        }
+        Database.instance = this;
+    }
+
+    static async getInstance(){
+        if (!Database.instance) {
+            const instance = new Database();
+            await instance.connect();
+            Database.instance = instance;
+        }
+        return Database.instance;
+    }
+
+    async connect(){
+        try {
+            this.#connection = await mysql.createConnection({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+            });
+            console.log('Connexion à la base de données réussie !');
+        } catch (error) {
+            console.error('Erreur lors de la connexion à la base de données :', error);
+            throw error;
+        }
+    }
+
+    async disconnect() {
+        try {
+            await this.#connection.end();
+            console.log('Déconnexion à la base de données réussie');
+        } catch (error) {
+            console.error('Erreur lors de la déconnexion à la base de données :', error);
+            throw error;
+        }
+    }
+
+    async query(query, values){
+        try {
+            const [rows] = await this.#connection.query(query, values);
+            return rows;
+        } catch (error) {
+            console.error('Erreur lors de la requête :', error);
+            throw error;
+        }
+    }
+}
+
+module.exports = Database;
