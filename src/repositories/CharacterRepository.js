3d78068a6c2d6cb37bf0f54fe91960bad18575be
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-05-22 11:17:14 +0200

    creation du repository du Character

---
+++ src/repositories/CharacterRepository.js
@@ -0,0 +1,34 @@
+const Database = require('../models/database');
+const CharacterEntity = require('../entities/CharacterEntity');
+
+class CharacterRepository {
+    static async getCharacterById(id) {
+        const db = await Database.getInstance();
+        const character = await db.query('SELECT * from personnage WHERE id = ?', [id]);
+        if (character.length === 0) return null;
+        return new CharacterEntity(
+            character[0].account_id,
+            character[0].id,
+            character[0].name,
+            character[0].divinity,
+            character[0].race,
+            character[0].xp,
+            character[0].sex);
+    }
+
+    static async getCharacterByName(name) {
+        const db = await Database.getInstance();
+        const character = await db.query('SELECT * from personnage WHERE lower(name) = ?', [name.toLowerCase()]);
+        if(character.length === 0) return null;
+        return new CharacterEntity(
+            character[0].account_id,
+            character[0].id,
+            character[0].name,
+            character[0].divinity,
+            character[0].race,
+            character[0].xp,
+            character[0].sex);
+    }
+}
+
+module.exports = CharacterRepository;
