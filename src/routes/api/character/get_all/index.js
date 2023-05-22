b1417c5ab051dd63ddd5159ac2591b95a409e578
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-05-22 11:14:53 +0200

    Suppression des dossiers avec le refactor

--- src/routes/api/character/get_all/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    res.json({code: 400, message: 'Bad Request'})
-});
-
-module.exports = router;
