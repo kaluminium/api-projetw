65ede917f9ad3fde9473f3aa081ba20a44c5fb70
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-05-26 19:04:49 +0200

    ajout de la route account

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const characterRouter = require('./character');
+const accountRouter = require('./account');
 
 router.get('/', (req, res) => {
     res.send('api');
 });
 
 router.use('/character', characterRouter);
+router.use('/account', accountRouter);
 
 module.exports = router;
