19b66063cd067077051f03338e44cae43aed54bd
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-05-26 15:18:52 +0200

    Changement du then

--- src/app.js
+++ src/app.js
@@ -2,20 +2,18 @@ const express = require('express');
 const Database = require("./models/Database");
 const apiRouter = require("./routes/api");
 
-const app = express();
-
-app.set('json spaces', 2);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.use("/api", apiRouter);
-
-app.listen(3000);
-
 Database.getInstance().then((db) => {
-  console.log("Database connected");
+  const app = express();
+
+  app.set('json spaces', 2);
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.use("/api", apiRouter);
+
+  app.listen(3000);
 }).catch((err) => {
   console.log("Database connection failed");
 });
